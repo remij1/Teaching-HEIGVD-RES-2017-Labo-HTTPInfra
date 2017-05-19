56d0a894fbf4daa5623e8f13af64dcf08d1ddff6
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-05-19 20:19:00 +0200

    Docker image with Node running dynamicaly (returning HackedComputers)

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,5 +1,47 @@
 var Chance = require('chance');
 var chance = new Chance();
 
+var express = require('express');
+var app = express();
 
-console.log("Bonjour " + chance.name());
+app.get('/', function (req, res) {
+    res.send(getHackedComputers());
+});
+
+app.listen(3000, function () {
+    console.log("Accepting HTTP requests on port 3000");
+});
+
+function getHackedComputers() {
+    var nbOfComputer = chance.integer({
+        min: 1, max: 10
+    });
+
+    var computers = [];
+
+    for (var s = 0; s < nbOfComputer; s++) {
+
+        var nbWebSite = chance.integer({
+            min: 1,
+            max: 5
+        });
+        var webSites = [];
+        for (var i = 0; i < nbWebSite; i++) {
+            webSites.push(chance.url());
+        }
+
+        var computer = {
+            'ip': chance.ip(),
+            'localisation': {
+                'lat': chance.latitude(),
+                'lon': chance.longitude()
+            },
+            'lastWebSitesVisited': webSites,
+            'lastPasswordUsed': chance.word() + chance.word()
+        };
+
+        computers.push(computer);
+    }
+
+    return computers;
+}
