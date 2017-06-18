633dfa1e700abf87e29c3c8f0d385c6a267b4a91
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2017-06-18 16:42:06 +0200

    It's now working ;-)

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -6,13 +6,6 @@
 ?>
 
 <VirtualHost *:80>
-    ServerName demo.res.ch
-
-    <Proxy "balancer://dynamic">
-        BalancerMember "http://<?php print $dynamic_app_1 ?>:3000"
-        BalancerMember "http://<?php print $dynamic_app_2 ?>:3000"
-        ProxySet lbmethod=bytraffic
-    </Proxy>
 
     <Proxy "balancer://static">
         BalancerMember "http://<?php print $static_app_1 ?>:80"
@@ -20,9 +13,17 @@
         ProxySet lbmethod=bytraffic
     </Proxy>
 
-    ProxyPass "/api/computers/" "balancer://dynamic"
-    ProxyPassReverse "/api/computers/" "balancer://dynamic"
+    <Proxy "balancer://dynamic">
+        BalancerMember "http://<?php print $dynamic_app_1 ?>:3000"
+        BalancerMember "http://<?php print $dynamic_app_2 ?>:3000"
+        ProxySet lbmethod=bytraffic
+    </Proxy>
 
-    ProxyPass "/" "balancer://static"
-    ProxyPassReverse "/" "balancer://static"
+    ServerName demo.res.ch
+
+    ProxyPass "/api/computers/" "balancer://dynamic/"
+    ProxyPassReverse "/api/computers/" "balancer://dynamic/"
+
+    ProxyPass "/" "balancer://static/"
+    ProxyPassReverse "/" "balancer://static/"
  </VirtualHost>
